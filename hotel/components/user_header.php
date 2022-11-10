1d17242d23bbc86da672b98b70a1d0a273e81f8f
Sanjyot Chatarkar <102315265+sanjyot21@users.noreply.github.com>
2022-11-11 02:04:30 +0530

    Update user_header.php

--- hotel/components/user_header.php
+++ hotel/components/user_header.php
@@ -1,7 +1,7 @@
 <!-- header section starts  -->
 <section class="header">
    <div class="flex">
-      <a href="#home" class="logo">VIT-HAR : Hotels And Resorts</a>
+      <a href="#home" class="logo">HAR : Hotels And Resorts</a>
       <a href="#availability" class="btn">Check Availability</a>
       <div id="menu-btn" class="fas fa-bars"></div>
    </div>
